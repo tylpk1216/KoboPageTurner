819b8e6ad3d0ec0dc4ccd69b8b3ee088435a966a
Michael Kuo <tylpk1216@gmail.com>
2020-07-20 20:36:06 +0800

    Update udev_server.sh

--- KoboServer/src/usr/local/koboserver/udev_server.sh
+++ KoboServer/src/usr/local/koboserver/udev_server.sh
@@ -7,14 +7,17 @@ then
     exit
 fi
 
-#load config
+# load config
 . $(dirname $0)/config.sh
 
-#create work dirs
+# create work dirs
 [ ! -e "$Logs" ] && mkdir -p "$Logs" >/dev/null 2>&1
 [ ! -e "$UserConfig" ] && echo #UNINSTALL > $UserConfig
 
-#output to log
+# copy files to $Logs
 [ ! -e "$Logs/$Server" ] && cp $KS_HOME/$Server $Logs
 [ ! -e "$Logs/$TestScript" ] && cp $KS_HOME/$TestScript $Logs
-$KS_HOME/run.sh >> $Logs/koboserver.log 2>&1 &
+[ ! -e "$Logs/run.sh" ] && cp $KS_HOME/run.sh $Logs
+
+# output to log
+$Logs/run.sh >> $Logs/koboserver.log 2>&1 &
