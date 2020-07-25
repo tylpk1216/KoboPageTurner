0eb674796cd7791dc8812bdf11460952c560f62b
tylpk1216 <tylpk1216@gmail.com>
2020-07-25 08:15:46 +0800

    Modify run.sh

--- KoboServer/src/usr/local/koboserver/run.sh
+++ KoboServer/src/usr/local/koboserver/run.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-#load config
+# load config
 . $(dirname $0)/config.sh
 
-#check if KoboServer contains the line "UNINSTALL"
+# check if KoboServer contains the line "UNINSTALL"
 if grep -q '^UNINSTALL$' $UserConfig; then
     echo "Uninstalling KoboServer!"
     $KS_HOME/uninstall.sh
     exit 0
 fi
 
-#$Logs/$Server &
-$Logs/$TestScript
+# check instance exist
+running="PID"
+if [ -e "$running" ]; then
+    echo "Server is running."
+    exit 0
+fi
+
+echo "Run" > $running
+$Logs/$Server
+#$Logs/$TestScript
 echo "`$Dt` done"
+rm $running
