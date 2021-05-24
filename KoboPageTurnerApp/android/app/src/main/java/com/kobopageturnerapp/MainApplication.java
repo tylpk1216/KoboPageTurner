23adb1d5bbe39a136e1c61514dff6f690d5f8c7e
Niels ten Boom <niels.ten.boom@rtl.nl>
2021-05-24 21:01:39 +0200

    no clue what all this is lol

--- KoboPageTurnerApp/android/app/src/main/java/com/kobopageturnerapp/MainApplication.java
+++ KoboPageTurnerApp/android/app/src/main/java/com/kobopageturnerapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.kobopageturner;
+package com.kobopageturnerapp;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.kobopageturner.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.kobopageturnerapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- KoboPageTurnerApp/ios/KoboPageTurnerAppTests/KoboPageTurnerAppTests.m
+++ KoboPageTurnerApp/ios/KoboPageTurnerAppTests/KoboPageTurnerAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface KoboPageTurnerTests : XCTestCase
+@interface KoboPageTurnerAppTests : XCTestCase
 
 @end
 
-@implementation KoboPageTurnerTests
+@implementation KoboPageTurnerAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
